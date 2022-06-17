f691f5785467f676f65ebfc712d98703c0255a5e
Gregory Man <man.gregory@gmail.com>
2022-06-17 14:27:53 +0300

    Added -c option to clear display
    Also released version 0.3 to clean screen on service stop

--- display.c
+++ display.c
@@ -1,17 +1,26 @@
-/*********************************************************************
-* Demo for SSD1306 I2C driver for Raspberry Pi 
-*********************************************************************/
 
-#include <unistd.h>      // sleep, usleep
+
+#include <unistd.h>      // sleep, usleep, getopt
+#include <stdlib.h>      // exit
 #include "ssd1306_i2c.h" // LCD_Display, ssd1306_begin
 
-int main(void)
+int main(int argc, char **argv)
 {
   unsigned short int count = 0;
 
   ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS); // LCD Screen initialization
   usleep(150000);                                           // Short delay to ensure the normal response of the lower functions
 
+  int opt;
+  while ((opt = getopt(argc, argv, "c")) != -1)
+  {
+    switch (opt)
+    {
+    case 'c': // clean screen and exit
+      OLED_Clear();
+      exit(0);
+    }
+  }
   LCD_DisplayInit();
 
   // Loop forever
